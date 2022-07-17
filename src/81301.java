61a0768ec36c1dd5dce6f07b7dd2c4aaae17b267
nayoong02 <sonyangel02@naver.com>
2022-07-18 02:06:06 +0900

    Lv.1_숫자 문자열과 영단어

--- src/81301.java
+++ src/81301.java
@@ -1,4 +1,4 @@
-class Solution {
+class Sol_81301 {
     public static int solution(String s) {
         String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
